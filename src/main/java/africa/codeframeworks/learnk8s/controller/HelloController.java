eb6bc2b852601791b2ef370b4f031d2f0f30e3b8
Amelye Chatila <amechax@gmail.com>
2023-08-05 10:46:56 +0300

    Initial Commit

---
+++ src/main/java/africa/codeframeworks/learnk8s/controller/HelloController.java
@@ -0,0 +1,11 @@
+package africa.codeframeworks.learnk8s.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
---
+++ src/test/java/africa/codeframeworks/learnk8s/Learnk8sApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.codeframeworks.learnk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learnk8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
